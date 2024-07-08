a32bdcb3edb59423d2e661e47fbc4d65345ba73b
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-07-08 16:04:07 +0100

    Create an interactive quiz

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function checkAnswer() {
+  let correctAnswer = 4;
+
+ let selectedRadioButton = document.querySelector('input[name="quiz"]:checked');
+ let userAnswer = selectedRadioButton.value;
+
+ let feedback = document.getElementById('feedback');
+
+ if (userAnswer == correctAnswer) {
+  feedback.textContent = "Correct! Well done.";
+ } else {
+  feedback.textContent = "That's incorrect. Try again!";
+ }
+}
+
+let submitButton = document.getElementById('submit-answer');
+        submitButton.addEventListener('click', checkAnswer);
