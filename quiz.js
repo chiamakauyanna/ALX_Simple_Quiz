f0b425f9e48d5326e3bb7f62af1ea95a6fff3d1f
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-07-08 16:48:34 +0100

    Correct basic quiz

--- quiz.js
+++ quiz.js
@@ -1,17 +1,17 @@
 function checkAnswer() {
-  let correctAnswer = 4;
+  let correctAnswer = "4";
 
- let selectedRadioButton = document.querySelector('input[name="quiz"]:checked');
- let userAnswer = selectedRadioButton.value;
+  let selectedRadioButton = document.querySelector('input[name="quiz"]:checked');
+  let userAnswer = selectedRadioButton ? selectedRadioButton.value : null;
 
- let feedback = document.getElementById('feedback');
+  let feedback = document.getElementById('feedback');
 
- if (userAnswer == correctAnswer) {
-  feedback.textContent = "Correct! Well done.";
- } else {
-  feedback.textContent = "That's incorrect. Try again!";
- }
+  if (userAnswer === correctAnswer) {
+      feedback.textContent = "Correct! Well done.";
+  } else {
+      feedback.textContent = "That's incorrect. Try again!";
+  }
 }
 
 let submitButton = document.getElementById('submit-answer');
-        submitButton.addEventListener('click', checkAnswer);
+submitButton.addEventListener('click', checkAnswer);
